43e58b200070fe31213b5e122935e1ed2b5fe914
Ahmed Attia <ahmadyattia@gmail.com>
2026-04-17 15:03:06 +0200

    created the products table and added it to the products page

---
+++ src/components/features/products/ProductTable.jsx
@@ -0,0 +1,57 @@
+// import { products } from "../../../data/products";
+
+const ProductTable = ({ products }) => {
+  console.log(products);
+  return (
+    <div className="bg-white rounded-xl flex justify-center">
+      <table className="w-full border-separate border-spacing-0 border border-gray-300 rounded-xl">
+        <thead>
+          <tr>
+            <th className="p-6 border-solid border-gray-300 border rounded-tl-xl">
+              Title
+            </th>
+            <th className="p-6 border-solid border-gray-300 border">id</th>
+            <th className="p-6 border-solid border-gray-300 border">Price</th>
+            <th className="p-6 border-solid border-gray-300 border">Stock</th>
+            <th className="p-6 border-solid border-gray-300 border rounded-tr-xl">
+              Status
+            </th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {products.map((product, index) => {
+            const isLast = index === products.length - 1;
+
+            return (
+              <tr className="">
+                <td
+                  className={`p-6 text-center border-solid border-gray-300 border ${isLast && "rounded-bl-xl"}`}
+                >
+                  <img src={product.image} alt={product.title} />
+                  <span>{product.title}</span>
+                </td>
+                <td className="p-6 text-center border-solid border-gray-300 border">
+                  {product.id}
+                </td>
+                <td className="p-6 text-center border-solid border-gray-300 border">
+                  ${product.price}
+                </td>
+                <td className="p-6 text-center border-solid border-gray-300 border">
+                  {product.stock}
+                </td>
+                <td
+                  className={`p-6 text-center border-solid border-gray-300 border ${isLast && "rounded-br-xl"}`}
+                >
+                  {product.status}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ProductTable;
--- src/pages/dashboard/Products.jsx
+++ src/pages/dashboard/Products.jsx
@@ -1,5 +1,17 @@
+import ProductTable from "../../components/features/products/ProductTable";
+import { products } from "../../data/products";
+import productsMapper from "../../data/mappers/productsMapper";
+
 const Products = () => {
-  return <h2 className="text-2xl font-bold mb-4">Products</h2>;
+  const mappedProducts = productsMapper(products);
+  console.log(mappedProducts);
+
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4">Products</h2>
+      <ProductTable products={mappedProducts} />
+    </div>
+  );
 };
 
 export default Products;
