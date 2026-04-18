5f433626b556a04f3683b97eaec36b1168e676d9
Ahmed Attia <ahmadyattia@gmail.com>
2026-04-18 12:46:40 +0200

    implemented row structure

--- src/components/features/orders/OrderRow.jsx
+++ src/components/features/orders/OrderRow.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const OrderRow = ({ row }) => {
-  return <div>OrderRow</div>;
+  return (
+    <tr>
+      <td>{row.id}</td>
+      <td>{row.customerName}</td>
+      <td>{row.customerEmail}</td>
+      <td>{row.date}</td>
+      <td>{row.total.formatted}</td>
+      <OrderStatusBadge status={row.status} />
+      <OrderStatusBadge status={row.paymentStatus} type="payment" />
+      <td>
+        <button>View</button>
+      </td>
+    </tr>
+  );
 };
 
 export default OrderRow;
