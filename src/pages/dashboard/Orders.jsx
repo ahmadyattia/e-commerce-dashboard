183765823776a3524f4e38e7f99aaa1bf1b50381
Ahmed Attia <ahmadyattia@gmail.com>
2026-04-20 20:04:00 +0200

    added .env

--- src/pages/dashboard/Orders.jsx
+++ src/pages/dashboard/Orders.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import OrderTable from "../../components/features/orders/OrderTable";
+import { useOrders } from "../../components/features/orders/hooks/useOrders";
 
 const Orders = () => {
+  const { orders, loading, error } = useOrders();
+
+  console.log(loading);
+  console.log(error);
+
+  console.log(orders);
+
   return (
     <div>
       <h2 className="text-2xl font-bold mb-4">Orders</h2>
