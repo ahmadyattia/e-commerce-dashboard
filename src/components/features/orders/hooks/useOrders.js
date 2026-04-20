fa7d0e1011bd7aaaabeb43dcaba3338f339c7ce3
Ahmed Attia <ahmadyattia@gmail.com>
2026-04-20 19:55:10 +0200

    fetching orders from db

---
+++ src/components/features/orders/hooks/useOrders.js
@@ -0,0 +1,32 @@
+import { onValue, orderByChild, query, ref } from "firebase/database";
+import { useEffect, useState } from "react";
+import { db } from "../../../../services/firebase";
+
+export const useOrders = () => {
+  const [orders, setOrders] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const ordersRef = ref(db, "/orders");
+
+    const unsubscribe = onValue(
+      ordersRef,
+      (snapshot) => {
+        if (snapshot.exists()) {
+          const allOrders = snapshot.val();
+          setOrders(allOrders);
+          setLoading(false);
+        }
+      },
+      (err) => {
+        setError(`Error fetching orders from the database: ${err}`);
+        setLoading(false);
+      },
+    );
+
+    return () => unsubscribe();
+  }, []);
+
+  return { orders, loading, error };
+};
