7d5a62f74f99bf013404c3433199eb4644eaf7ce
Ahmed Attia <ahmadyattia@gmail.com>
2026-04-21 21:26:06 +0200

    connected products from firebase

--- src/pages/dashboard/Products.jsx
+++ src/pages/dashboard/Products.jsx
@@ -1,15 +1,17 @@
 import ProductTable from "../../components/features/products/ProductTable";
-import { products } from "../../data/products";
 import productsMapper from "../../data/mappers/productsMapper";
+import { useProducts } from "../../components/features/products/hooks/useProducts";
 
 const Products = () => {
+  const { products, error, loading } = useProducts();
   const mappedProducts = productsMapper(products);
-  console.log(mappedProducts);
 
   return (
     <div>
       <h2 className="text-2xl font-bold mb-4">Products</h2>
-      <ProductTable products={mappedProducts} />
+      {loading && <p>Loading products...</p>}
+      {error && <p>Error while fetching products...</p>}
+      {products && <ProductTable products={mappedProducts} />}
     </div>
   );
 };
